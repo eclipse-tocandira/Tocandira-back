020f0d8abbfb39eaebb863b79cefa10adef2c771
Pedro Ricardo C. Souza <pricardo@aimirimsti.com.br>
2022-10-20 08:54:30 -0300

    fix(src): Insert SQL url as enviroment variable.

--- src/env.py
+++ src/env.py
@@ -15,7 +15,10 @@ class Enviroment:
     '''
     API_NAME = os.getenv('CONF_API_NAME', default='api')
     '''`API_NAME` (str): The name of the API route as wanted in 
-    `/{name}/{version}`'''
+    `/{name}/{version}`. Default is `"api"`'''
     API_VERSION = os.getenv('CONF_API_VERSION', default='v0')
     '''`API_NAME` (str): The version of the API route as wanted in
-    `/{name}/{version}`'''
+    `/{name}/{version}`. Default is `"v0"`'''
+    DATABASE_URL = os.getenv('CONF_DATABASE_URL', default='sqlite:///db/sql_app.db')
+    '''`DATABASE_URL` (str): The SQL URL to find the `sql_app.db` file.
+    Default is `"sqlite:///db/sql_app.db"`'''
