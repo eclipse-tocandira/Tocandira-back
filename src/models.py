fc2a97153a19ab31b8f1f2b80fb1ee81e9c26569
Pedro Ricardo C. Souza <pricardo@aimirimsti.com.br>
2022-11-22 13:25:28 -0300

    feat(src): Insert PLC datasource and datapoint models for the DB.

--- src/models.py
+++ src/models.py
@@ -7,7 +7,9 @@ Copyright (c) 2017 Aimirim STI.\n
 '''
 
 # Import system libs
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Column, ForeignKey, Boolean, Integer, String
+from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy.orm import relationship
 
 # Import custom libs
 from .database import Base
@@ -21,3 +23,117 @@ class User(Base):
     name = Column(String, index=True)
     password = Column(String)
     is_admin = Column(Boolean, default=True)
+
+# --------------------
+class DataPoint(Base):
+    __tablename__ = "datapoints"
+    id = Column(Integer, primary_key=True, index=True)
+    # Items
+    name = Column(String)
+    description = Column(String)
+    num_type = Column(String)
+    access = Column(String, nullable=False)
+    # Other tables
+    datasource = relationship("DataSource", back_populates="datapoints")# N to 1
+    datasource_id = Column(Integer, ForeignKey("datasources.id"))
+    # Allow inheritance
+    __mapper_args__ = {'polymorphic_on': access}
+# --------------------
+
+# --------------------
+class DataSource(Base):
+    __tablename__ = "datasources"
+    id = Column(Integer, primary_key=True, index=True)
+    # Items
+    name = Column(String)
+    plc_ip = Column(String)
+    plc_port = Column(Integer)
+    cycletime = Column(Integer)
+    timeout = Column(Integer)
+    status = Column(Boolean)
+    # Other tables
+    protocol = relationship("Protocol", back_populates="datasource", uselist=False)# 1 to 1
+    datapoints = relationship("DataPoint", back_populates="datasource")# 1 to N
+# --------------------
+
+# --------------------
+class Protocol(Base):
+    __tablename__ = "protocols"
+    id = Column(Integer, primary_key=True, index=True)
+    # Items
+    name = Column(String, nullable=False)
+    # Other tables
+    datasource = relationship("DataSource", back_populates="protocol")# 1 to 1
+    datasource_id = Column(Integer, ForeignKey("datasources.id"))
+    # Allow inheritance
+    __mapper_args__ = {'polymorphic_on': name,'polymorphic_identity' : 'protocol'}
+# --------------------
+
+# --------------------
+class ProtSiemens(Protocol):
+    # Inheritance info
+    __mapper_args__ = {'polymorphic_identity': 'Siemens'}
+    # Items
+    @declared_attr
+    def rack(cls):
+        return Protocol.__table__.c.get('rack', Column(Integer))
+    @declared_attr
+    def slot(cls):
+        return Protocol.__table__.c.get('slot', Column(Integer))
+    @declared_attr
+    def plc(cls):
+        return Protocol.__table__.c.get('plc', Column(String))
+
+class DataSiemens(DataPoint):
+    # Inheritance info
+    __mapper_args__ = {'polymorphic_identity': 'Siemens'}
+    # Items
+    @declared_attr
+    def address(cls):
+        return DataPoint.__table__.c.get('address', Column(String))
+# --------------------
+
+# --------------------
+class ProtRockwell(Protocol):
+    # Inheritance info
+    __mapper_args__ = {'polymorphic_identity': 'Rockwell'}
+    # Items
+    @declared_attr
+    def path(cls):
+        return Protocol.__table__.c.get('path', Column(String))
+    @declared_attr
+    def slot(cls):
+        return Protocol.__table__.c.get('slot', Column(Integer))
+    @declared_attr
+    def connection(cls):
+        return Protocol.__table__.c.get('connection', Column(String))
+
+class DataRockwell(DataPoint):
+    # Inheritance info
+    __mapper_args__ = {'polymorphic_identity': 'Rockwell'}
+    # Items
+    @declared_attr
+    def tag_name(cls):
+        return DataPoint.__table__.c.get('tag_name', Column(String))
+# --------------------
+
+# --------------------
+class ProtModbus(Protocol):
+    # Inheritance info
+    __mapper_args__ = {'polymorphic_identity': 'Modbus'}
+    # Items
+    @declared_attr
+    def slave_id(cls):
+        return Protocol.__table__.c.get('slave_id', Column(Integer))
+
+class DataModbus(DataPoint):
+    # Inheritance info
+    __mapper_args__ = {'polymorphic_identity': 'Modbus'}
+    # Items
+    @declared_attr
+    def func_code(cls):
+        return DataPoint.__table__.c.get('func_code', Column(Integer))
+    @declared_attr
+    def address(cls):
+        return DataPoint.__table__.c.get('address', Column(Integer))
+# --------------------
