51e7b6a855f93edf0631ee32b55d6529de1cc422
Pedro Ricardo C. Souza <pricardo@aimirimsti.com.br>
2022-11-23 16:52:04 -0300

    feat(plc_data): Only authorize access to routes for logged user.

--- src/plc_data/routes.py
+++ src/plc_data/routes.py
@@ -8,14 +8,14 @@ Copyright (c) 2017 Aimirim STI.\n
 '''
 
 # Import system libs
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
 
 # Import custom libs
 from . import schemas
-from .. import models
 from ..database import get_db
 from ..crud import Tplcdata
+from ..user_auth import routes as usr_routes
 
 #######################################
 
@@ -25,7 +25,7 @@ from ..crud import Tplcdata
 #       user shoud pass, but it is handled internaly by FastAPI.
 
 # --------------------
-def get_protocol_defaults():
+def get_protocol_defaults(usr:str = Depends(usr_routes._check_valid_token)):
     ''' Get the list of DataSource protocols available.\n
     return `val_list` (JSONResponse): A `schemas.simpleList` object
     automatically parsed into an HTTP_OK response.\n
@@ -40,7 +40,7 @@ def get_protocol_defaults():
 # --------------------
 
 # --------------------
-def get_datasource_defaults(prot_name:str):
+def get_datasource_defaults(prot_name:str, usr:str = Depends(usr_routes._check_valid_token)):
     ''' Get the list of DataSource information to use as placeholders.\n
     `prot_name` (str): Protocol name to ger defauts from.\n
     return `val_dict` (JSONResponse): A `schemas.dataSourceInfo` object
@@ -56,7 +56,7 @@ def get_datasource_defaults(prot_name:str):
 # --------------------
 
 # --------------------
-def create_datasource(datasource:schemas.dataSourceInfo, db:Session=Depends(get_db)):
+def create_datasource(datasource:schemas.dataSourceInfo, db:Session=Depends(get_db), usr:str = Depends(usr_routes._check_valid_token)):
     ''' Create an entry at in database to save this DataSource.\n
     `datasource` (schemas.dataSourceInfo): DataSource informations.\n
     return `val_ds` (JSONResponse): A `schemas.dataSource` automatically parser into
@@ -72,7 +72,7 @@ def create_datasource(datasource:schemas.dataSourceInfo, db:Session=Depends(get_
 # --------------------
 
 # --------------------
-def get_datasource_by_name(ds_name:str, db:Session=Depends(get_db)):
+def get_datasource_by_name(ds_name:str, db:Session=Depends(get_db), usr:str = Depends(usr_routes._check_valid_token)):
     ''' Search an entry in database with provided name.\n
     `ds_name` (str): DataSource name.\n
     return `val_ds` (JSONResponse): A `schemas.dataSource` automatically parser into
@@ -88,7 +88,7 @@ def get_datasource_by_name(ds_name:str, db:Session=Depends(get_db)):
 # --------------------
 
 # --------------------
-def get_datasources(db:Session=Depends(get_db)):
+def get_datasources(db:Session=Depends(get_db), usr:str = Depends(usr_routes._check_valid_token)):
     ''' Get all datasource entries in database.\n
     return `val_ds` (JSONResponse): A list of `schemas.dataSource` automatically parser into
     a HTTP_OK response.\n
@@ -103,7 +103,7 @@ def get_datasources(db:Session=Depends(get_db)):
 # --------------------
 
 # --------------------
-def get_datasources_by_range(ini:int, end:int, db:Session=Depends(get_db)):
+def get_datasources_by_range(ini:int, end:int, db:Session=Depends(get_db), usr:str = Depends(usr_routes._check_valid_token)):
     ''' Get all datasource entries in database.\n
     `ini` (int): First query result to show. Starts at `1`.\n
     `end` (int): Last query result to show, inclusive. \n
