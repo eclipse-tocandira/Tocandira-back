f3ba28a41b46e4f4ac537a091b6f90e0f46c5f89
Pedro Ricardo C. Souza <pricardo@aimirimsti.com.br>
2023-09-11 10:29:20 -0300

    feat(user_auth): Insert user management routes.

--- src/models.py
+++ src/models.py
@@ -20,9 +20,10 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    password = Column(String)
-    is_admin = Column(Boolean, default=True)
+    name = Column(String, index=True, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    change_password = Column(Boolean, default=False)
+    is_admin = Column(Boolean, default=False)
 
 class Collector(Base):
     __tablename__ = "collector"
